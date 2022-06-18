9607f971b1c731d99bd437617841e5c27aac5084
Romand2k3 <74187365+Romand2k3@users.noreply.github.com>
2022-06-18 10:56:06 +0300

    7th

---
+++ cmake-build-debug/external/doctest/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: D:/cppsha/LabyrinthRomand/external/doctest
+# Build directory: D:/cppsha/LabyrinthRomand/cmake-build-debug/external/doctest
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
