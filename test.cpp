87c2935100c1424bf239df0d518cdfbd9ceee224
Romand2k3 <74187365+Romand2k3@users.noreply.github.com>
2022-06-18 12:28:33 +0300

    11th

--- test.cpp
+++ test.cpp
@@ -2,11 +2,12 @@
 #include "calculate.h"
 
 TEST_CASE("onmap") {
-    int v = OnMap(50, 50, 200, 200);
-
-    REQUIRE(v == true);
+    REQUIRE(OnMap(50, 50, 200, 200) == true);
+    REQUIRE(OnMap(250, 250, 200, 200) == false);
 }
 
 TEST_CASE("pyrahit") {
 REQUIRE(PyraHit(5, 5, 10, 10) == false);
+REQUIRE(PyraHit(5, 5, 5, 5) == true);
+REQUIRE(PyraHit(5, 5, 6, 4) == true);
 }
